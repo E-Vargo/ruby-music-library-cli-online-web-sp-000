1bd7aac874ab4366299f5eb91cb3bf343d2d14f4
Elliott Vargo <vargoelliott@gmail.com>
2021-05-19 23:35:20 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,25 +2,6 @@ class Song
 
 attr_accessor :name
 
-@@all = []
-
-def initialize(name)
-@name = name
-save
-end
-
-def save
-@@all << self
-end
-
-def self.all
-  @@all
-end
-
-def self.destroy_all
-  @@all.clear
-end
-
-
+include Total
 
 end
